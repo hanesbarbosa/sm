21cdd175f67bedc5f25c39754f109d52aa909cad
Hanes Oliveira <hanes@hispersonal.email>
2025-08-29 10:03:03 -0600

    Create initial Slot Machine transform of composites.

---
+++ smt/transform.go
@@ -0,0 +1,38 @@
+package smt
+
+const BASES = 4
+
+func isNotSpecialCode(c uint8) bool {
+	// Isolate first bit.
+	b1 := c >> 7
+	// Isolate second bit.
+	b2 := c << 1
+	b2 >>= 7
+	// NAND.
+	return !(b1 == 1 && b2 == 1)
+}
+
+func substring(c uint8, i int) uint8 {
+	// Left offset.
+	c <<= (i % BASES) * 2
+	// Right offset.
+	return c >> 6
+}
+
+func transform(c uint8) uint8 {
+	// Only transform secial codes.
+	if isNotSpecialCode(c) {
+		// Instruction.
+		i := substring(c, 0)
+		// Substrings.
+		c1, c2, c3 := substring(c, 1), substring(c, 2), substring(c, 3)
+		// Transformations.
+		c1Prime, c2Prime, c3Prime := c1^c3, c3, c2^c3
+		// Iterate instruction.
+		i = (i + 1) % (BASES - 1)
+		// Pack results into a composite.
+		return (i << 6) ^ (c1Prime << 4) ^ (c2Prime << 2) ^ c3Prime
+	} else {
+		return c
+	}
+}
